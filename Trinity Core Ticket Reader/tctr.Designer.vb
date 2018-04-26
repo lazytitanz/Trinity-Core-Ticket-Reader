a46adf9d483e47aaf17ab1a413202f761e917feb
lazytitanz <38267944+lazytitanz@users.noreply.github.com>
2018-04-26 13:32:07 -0500

    tctr

--- Trinity Core Ticket Reader/tctr.Designer.vb
+++ Trinity Core Ticket Reader/tctr.Designer.vb
@@ -23,13 +23,23 @@ Partial Class tctr
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(tctr))
+        Me.webbrowser = New System.Windows.Forms.Panel()
         Me.SuspendLayout()
+        '
+        'webbrowser
+        '
+        Me.webbrowser.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.webbrowser.Location = New System.Drawing.Point(0, 0)
+        Me.webbrowser.Name = "webbrowser"
+        Me.webbrowser.Size = New System.Drawing.Size(816, 451)
+        Me.webbrowser.TabIndex = 0
         '
         'tctr
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(816, 451)
+        Me.Controls.Add(Me.webbrowser)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "tctr"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
@@ -38,4 +48,5 @@ Partial Class tctr
 
     End Sub
 
+    Friend WithEvents webbrowser As Panel
 End Class
--- Trinity Core Ticket Reader/tctr.vb
+++ Trinity Core Ticket Reader/tctr.vb
@@ -1,3 +1,20 @@
-﻿Public Class tctr
+﻿Imports CefSharp
+Imports CefSharp.Winform
+Public Class tctr
 
+    Private WithEvents browser As WinForms.ChromiumWebBrowser
+
+    Public Sub New()
+        InitializeComponent()
+
+        Dim settings As New CefSettings()
+        CefSharp.Cef.Initialize(settings)
+
+        browser = New WinForms.ChromiumWebBrowser("http://localhost/proj/ticketreader/index.php") With {
+            .Dock = DockStyle.Fill
+        }
+        webbrowser.Controls.Add(browser)
+
+
+    End Sub
 End Class
